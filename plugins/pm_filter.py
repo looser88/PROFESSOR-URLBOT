7d16663c054a642c9dafe719d0a6c840f60e63c3
looser88 <98893135+looser88@users.noreply.github.com>
2023-02-23 21:02:56 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -110,7 +110,7 @@ async def pm_AutoFilter(client, msg, pmspoll=False):
             **locals()
         )
     else:
-        cap = f"Here is what i found for your query {search}"
+        cap = f"<b>🎞️ᴍᴏᴠɪᴇ ɴᴀᴍᴇ ☞ {search}\n\n🎯Rᴇǫᴜᴇsᴛᴇᴅ Bʏ ☞ {}\n\n‣ Tʜɪs Mᴇssᴀɢᴇ Wɪʟʟ ʙᴇ Aᴜᴛᴏ-Dᴇʟᴇᴛᴇᴅ Aғᴛᴇʀ 05 Mɪɴᴜᴛᴇs.<b>"
     if imdb and imdb.get('poster'):
         try:
             hehe = await message.reply_photo(photo=imdb.get('poster'), caption=cap, reply_markup=InlineKeyboardMarkup(btn))
