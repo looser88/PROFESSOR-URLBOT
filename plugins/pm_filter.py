c121518445f24f91ca23818b1e32cfbfacd84028
looser88 <98893135+looser88@users.noreply.github.com>
2023-02-25 11:29:40 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -174,7 +174,7 @@ async def pm_spoll_choker(msg):
     movielist += [(re.sub(r'(\-|\(|\)|_)', '', i, flags=re.IGNORECASE)).strip() for i in gs_parsed]
     movielist = list(dict.fromkeys(movielist))  # removing duplicates
     if not movielist:
-        k = await msg.reply("I couldn't find anything related to that. Check your spelling")
+        k = await msg.reply("I couldn't find anything related to that. Check your spelling", reply_markup=InlineKeyboardMarkup(btn2))
         await asyncio.sleep(8)
         await k.delete()
         return
